f52b6bc4cf59c1e0d85066928af050db91820b17
Adrien Durand <adrien-durand@live.fr>
2016-09-01 09:10:59 +0200

    grille de tirs affichée

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,15 @@
 public class Main {
 	public static void main(String[] args) {
 		Grille grilleJoueur = new Grille();
-		
+		Grille grilleTirs = new Grille();
+		grilleTirs.afficherGrille();
+		System.out.println();
+		for (int j = 0; j < 43; j++) {
+			System.out.print("_");
+		}
+		System.out.println();
+		System.out.println();
+		System.out.println();
 		grilleJoueur.afficherGrille();
 	}
 }
