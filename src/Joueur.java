931e63831fc50d1dc666004619f39c87714c7179
LelongBenjamin <lelongben50@outlook.fr>
2016-09-01 14:14:47 +0200

    orientation bateau

--- src/Joueur.java
+++ src/Joueur.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Joueur {
 
@@ -9,28 +10,44 @@ public class Joueur {
 		grille.afficherGrille();
 		System.out.print("Donnez les coordonnees pour le bateau de 5 : ");
 		int[] cood = TourJoueur.demanderCoordonnees();
-		grille.placerBateau(new Bateau(5, cood[1], cood[0], demanderOrientation()));
+		boolean orientation = demanderOrientation();
+		grille.placerBateau(new Bateau(5, cood[1], cood[0], orientation));
 		grille.afficherGrille();
 		System.out.print("Donnez les coordonnees pour le bateau de 4 : ");
 		cood = TourJoueur.demanderCoordonnees();
-		grille.placerBateau(new Bateau(4, cood[1], cood[0], true));
+		orientation = demanderOrientation();
+		grille.placerBateau(new Bateau(4, cood[1], cood[0], orientation));
 		grille.afficherGrille();
 		System.out.print("Donnez les coordonnees pour le bateau de 3 : ");
 		cood = TourJoueur.demanderCoordonnees();
-		grille.placerBateau(new Bateau(3, cood[1], cood[0], true));
+		orientation = demanderOrientation();
+		grille.placerBateau(new Bateau(3, cood[1], cood[0], orientation));
 		grille.afficherGrille();
 		System.out.print("Donnez les coordonnees pour le bateau de 3 : ");
 		cood = TourJoueur.demanderCoordonnees();
-		grille.placerBateau(new Bateau(3, cood[1], cood[0], true));
+		orientation = demanderOrientation();
+		grille.placerBateau(new Bateau(3, cood[1], cood[0], orientation));
 		grille.afficherGrille();
 		System.out.print("Donnez les coordonnees pour le bateau de 2 : ");
 		cood = TourJoueur.demanderCoordonnees();
-		grille.placerBateau(new Bateau(2, cood[1], cood[0], true));
+		orientation = demanderOrientation();
+		grille.placerBateau(new Bateau(2, cood[1], cood[0], orientation));
 		grille.afficherGrille();
 	}
 	
 	public boolean demanderOrientation(){
 		System.out.print("\n (1) verticale (2) horizontal : ");
+		Scanner sc = new Scanner(System.in);
+		String mess = sc.nextLine();
+		
+		if(mess.length() == 1 && ( mess.charAt(0) == '1' || mess.charAt(0) == '2')){
+			if(mess.charAt(0) == '1'){
+				return true;
+			}
+			return false;
+		}else{
+			demanderOrientation();
+		}
 		return false;
 	}
 	
