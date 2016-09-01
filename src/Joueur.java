8d0f97246736f1f5cea8003d3d59007756c6a77d
LelongBenjamin <lelongben50@outlook.fr>
2016-09-01 11:39:12 +0200

    up bateau

--- src/Joueur.java
+++ src/Joueur.java
@@ -7,8 +7,26 @@ public class Joueur {
 	
 	public Joueur(Grille grille) {
 		grille.afficherGrille();
+		System.out.print("Donnez les coordonnees pour le bateau de 5 : ");
 		int[] cood = TourJoueur.demanderCoordonnees();
+		grille.placerBateau(new Bateau(5, cood[1], cood[0], true));
+		grille.afficherGrille();
+		System.out.print("Donnez les coordonnees pour le bateau de 4 : ");
+		cood = TourJoueur.demanderCoordonnees();
+		grille.placerBateau(new Bateau(4, cood[1], cood[0], true));
+		grille.afficherGrille();
+		System.out.print("Donnez les coordonnees pour le bateau de 3 : ");
+		cood = TourJoueur.demanderCoordonnees();
 		grille.placerBateau(new Bateau(3, cood[1], cood[0], true));
+		grille.afficherGrille();
+		System.out.print("Donnez les coordonnees pour le bateau de 3 : ");
+		cood = TourJoueur.demanderCoordonnees();
+		grille.placerBateau(new Bateau(3, cood[1], cood[0], true));
+		grille.afficherGrille();
+		System.out.print("Donnez les coordonnees pour le bateau de 2 : ");
+		cood = TourJoueur.demanderCoordonnees();
+		grille.placerBateau(new Bateau(2, cood[1], cood[0], true));
+		grille.afficherGrille();
 	}
 	
 	/**
