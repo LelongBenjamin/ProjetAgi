48257b28b0826c363b3ce5ee331fded0767f5e9e
LelongBenjamin <lelongben50@outlook.fr>
2016-09-01 11:01:14 +0200

    Tour continu

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
 
 public class Main {
 	public static void main(String[] args) {
+		
+		Joueur joueur = new Joueur();
+		
 		Grille grilleJoueur = new Grille();
 		Grille grilleIa = new Grille();
 		
-		new TourJoueur(grilleJoueur, grilleIa);
+		while(true){
+			new TourJoueur(grilleJoueur, grilleIa);
+		}
 	}
 }
