380ff2cf27d59578ac3885d592d43147b0932f55
Adrien Durand <adrien-durand@live.fr>
2016-09-01 14:27:06 +0200

    creations des tours de jeu

--- src/Main.java
+++ src/Main.java
@@ -6,24 +6,40 @@ public class Main {
 
 		Grille grilleJoueur = new Grille();
 		Grille grilleIa = new Grille();
-		
+
 		Joueur joueur = new Joueur(grilleJoueur);
-		
-		Random r=new Random();
-		int random=r.nextInt(2);
-		if(random==1){
-			System.out.println("Joueur commence");
-		}else{
-			System.out.println("Ia commence");
+		Joueur ordi = new Joueur(grilleIa);
+
+		int cpt = 0;
+
+		if (new Random().nextInt(2) == 1) {
+			cpt = 2;
+		} else {
+			cpt = 1;
 		}
-		
-			
-		while(true){
-			new TourJoueur(grilleJoueur, grilleIa);
+
+		while (joueur.bateauAlive() >= 1 && ordi.bateauAlive() >= 1) {
+
+			clearTerminal();
+
+			if (cpt % 2 == 0) {
+				if (cpt == 2) {
+					System.out.println("Le joueur commence\n");
+					cpt = 4;
+				}
+				new TourJoueur(grilleJoueur, grilleIa);
+			} else {
+				if (cpt == 1) {
+					System.out.println("L'ordinateur commence\n");
+					cpt = 3;
+				}
+				new TourIa(grilleJoueur);
+			}
+			cpt++;
 		}
 	}
-	
-	static void clearTerminal(){
+
+	static void clearTerminal() {
 		for (int i = 0; i < 42; i++) {
 			System.out.print('\n');
 		}
--- src/TourIa.java
+++ src/TourIa.java
@@ -4,13 +4,14 @@ public class TourIa {
 
 	public TourIa(Grille grilleJoueur) {
 
+		System.out.println("C'est le tour de l'ordinateur!");
 		int[] cood = coorAleatoires();		
 		grilleJoueur.tirer(cood[1], cood[0]);
 	}
 	
 	private int[] coorAleatoires() {
 		
-		return new int[]{new Random().nextInt(11), new Random().nextInt(11)};
+		return new int[]{new Random().nextInt(10), new Random().nextInt(10)};
 		
 	}
 
