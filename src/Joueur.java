1215741ed33ca470f534a30565599932dfe4ad05
LelongBenjamin <lelongben50@outlook.fr>
2016-09-01 14:23:32 +0200

    pour la beaute du code 5

--- src/Joueur.java
+++ src/Joueur.java
@@ -7,31 +7,18 @@ public class Joueur {
 	private List<Bateau> listeBateau = new ArrayList<>();
 	
 	public Joueur(Grille grille) {
-		grille.afficherGrille();
-		System.out.print("Donnez les coordonnees pour le bateau de 5 : ");
-		int[] cood = TourJoueur.demanderCoordonnees();
-		boolean orientation = demanderOrientation();
-		grille.placerBateau(new Bateau(5, cood[1], cood[0], orientation));
-		grille.afficherGrille();
-		System.out.print("Donnez les coordonnees pour le bateau de 4 : ");
-		cood = TourJoueur.demanderCoordonnees();
-		orientation = demanderOrientation();
-		grille.placerBateau(new Bateau(4, cood[1], cood[0], orientation));
-		grille.afficherGrille();
-		System.out.print("Donnez les coordonnees pour le bateau de 3 : ");
-		cood = TourJoueur.demanderCoordonnees();
-		orientation = demanderOrientation();
-		grille.placerBateau(new Bateau(3, cood[1], cood[0], orientation));
-		grille.afficherGrille();
-		System.out.print("Donnez les coordonnees pour le bateau de 3 : ");
-		cood = TourJoueur.demanderCoordonnees();
-		orientation = demanderOrientation();
-		grille.placerBateau(new Bateau(3, cood[1], cood[0], orientation));
-		grille.afficherGrille();
-		System.out.print("Donnez les coordonnees pour le bateau de 2 : ");
-		cood = TourJoueur.demanderCoordonnees();
-		orientation = demanderOrientation();
-		grille.placerBateau(new Bateau(2, cood[1], cood[0], orientation));
+		
+		int[] tab = new int[]{5,4,3,3,2};
+		
+		for (int i = 0; i < tab.length; i++) {
+			Main.clearTerminal();
+			grille.afficherGrille();
+			System.out.print("Donnez les coordonnees pour le bateau de " + tab[i] + ": ");
+			int[] cood = TourJoueur.demanderCoordonnees();
+			boolean orientation = demanderOrientation();
+			grille.placerBateau(new Bateau(tab[i], cood[1], cood[0], orientation));
+		}
+		Main.clearTerminal();
 		grille.afficherGrille();
 	}
 	
