7a19f90bc945d8a129ea6ae4646ce3cffdb0fcff
LelongBenjamin <lelongben50@outlook.fr>
2016-09-01 11:28:58 +0200

    pour la beaute du code 4

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,12 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		
-<<<<<<< HEAD
+
 		Grille grilleJoueur = new Grille();
 		Grille grilleIa = new Grille();
 		
 		Joueur joueur = new Joueur(grilleJoueur);
-=======
+		
 		Random r=new Random();
 		int random=r.nextInt(2);
 		if(random==1){
@@ -18,15 +17,7 @@ public class Main {
 			System.out.println("Ia commence");
 		}
 		
-		
-		
-		Joueur joueur = new Joueur();
-		
-		Grille grilleJoueur = new Grille();
-		Grille grilleIa = new Grille();
-		
->>>>>>> 221076e919da9d9682c740da5987a865b7104336
-		
+			
 		while(true){
 			new TourJoueur(grilleJoueur, grilleIa);
 		}
