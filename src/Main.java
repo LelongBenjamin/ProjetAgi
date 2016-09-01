221076e919da9d9682c740da5987a865b7104336
Adrien Durand <adrien-durand@live.fr>
2016-09-01 11:21:03 +0200

    random du commencement

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,24 @@
+import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
+		
+		Random r=new Random();
+		int random=r.nextInt(2);
+		if(random==1){
+			System.out.println("Joueur commence");
+		}else{
+			System.out.println("Ia commence");
+		}
+		
+		
 		
 		Joueur joueur = new Joueur();
 		
 		Grille grilleJoueur = new Grille();
 		Grille grilleIa = new Grille();
+		
 		
 		while(true){
 			new TourJoueur(grilleJoueur, grilleIa);
