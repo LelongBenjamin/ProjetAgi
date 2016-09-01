1b94ab19b22f36255dea19e5b39a132ad1470d75
Adrien Durand <adrien-durand@live.fr>
2016-09-01 17:11:10 +0200

    crashtest3

--- src/TourIa.java
+++ src/TourIa.java
@@ -1,34 +1,30 @@
-import java.util.List;
+
 import java.util.Random;
-import java.util.ArrayList;
+
 
 public class TourIa {
 	
-	static List <int []> tirFait = new ArrayList<>();
 
 	public TourIa(Grille grilleJoueur, Joueur joueur, Joueur ordi) {
 		System.out.println("C'est le tour de l'ordinateur!");
-		int[] cood = coorAleatoires();		
+		int[] cood = coorAleatoires();
+		boolean bon = false;
+		while(!bon){
+			if(grilleJoueur.getCase(cood[1],cood[0]) == Case.TIRRATE && grilleJoueur.getCase(cood[1],cood[0]) == Case.TIRTOUCHE){
+				cood = coorAleatoires();
+			}else{
+				bon = true;
+			}
+		}
+		
 		grilleJoueur.tirer(cood[1], cood[0], joueur);
-		tirFait.add(cood);
 	}
 	
 	static int[] coorAleatoires() {
-		int coordonnee1;
-		int coordonnee2;
-		coordonnee1=new Random().nextInt(10); 
-		coordonnee2=new Random().nextInt(10);
-		int[] proposition=new int[2];
-		proposition[0]=coordonnee1;
-		proposition[1]=coordonnee2;
-		for (int i = 0; i < tirFait.size(); i++) {
-			if(proposition.equals(tirFait.get(i))){
-				coorAleatoires();
-			}
-		}
+		
+		int[] proposition=new int[]{new Random().nextInt(10), new Random().nextInt(10)};
 		return proposition;
 		
 	}
-	
 
 }
