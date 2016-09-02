a2f4ea3c5fb9b57202532c09504da570fff8e390
Raphael Chabeniuk <rafachab@gmail.com>
2016-09-02 09:29:41 +0200

    MaJ de TestBateau

--- src/TestBateau.java
+++ src/TestBateau.java
@@ -13,5 +13,18 @@ public class TestBateau {
 		assertEquals(Bateau.Horizontal, boat2.isOrientation());
 		
 	}
+	
+	@Test
+	public void testEstCoule() {
+		
+		Grille grille = new Grille();
+		Bateau boat1 = new Bateau(4,0,0,Bateau.Vertical);
+		Bateau boat2 = new Bateau(0,0,1,Bateau.Horizontal);
+		grille.placerBateau(boat1);
+		grille.placerBateau(boat2);
+		assertFalse(boat1.estCoule(grille));
+		assertTrue(boat2.estCoule(grille));
+		
+	}
 
 }
